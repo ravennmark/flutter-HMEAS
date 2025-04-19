97d3dcfb87c62f9c627c3b079cdfb4b213f9ea6a
ravennmark <rmrm2022-1390-82870@bicol-u.edu.ph>
2025-04-19 22:48:11 +0800

    added icons

--- hmeas/lib/ui/dashboard/dashboard_screen.dart
+++ hmeas/lib/ui/dashboard/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -8,6 +9,15 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _icons = [
+    const FaIcon(FontAwesomeIcons.house, size: 22),
+    const FaIcon(FontAwesomeIcons.heartPulse, size: 22),
+    const FaIcon(FontAwesomeIcons.starOfLife, size: 22),
+    const FaIcon(FontAwesomeIcons.user, size: 22),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,9 +36,67 @@ class _DashboardScreenState extends State<DashboardScreen> {
           ),
         ),
         centerTitle: true,
+        backgroundColor: Colors.white,
       ),
-
-      body: Padding(padding: const EdgeInsets.symmetric(horizontal: 24)),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: SizedBox(),
+      ),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(32),
+            topRight: Radius.circular(32),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 8,
+              offset: Offset(0, -2),
+            ),
+          ],
+        ),
+        child: BottomAppBar(
+          elevation: 0,
+          color: Colors.transparent,
+          child: SizedBox(
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: List.generate(_icons.length, (index) {
+                bool isSelected = index == _selectedIndex;
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _selectedIndex = index;
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration:
+                        isSelected
+                            ? BoxDecoration(
+                              color: const Color(0xFF0D1B63),
+                              shape: BoxShape.circle,
+                            )
+                            : null,
+                    alignment: Alignment.center,
+                    child: IconTheme(
+                      data: IconThemeData(
+                        color:
+                            isSelected ? Colors.white : const Color(0xFF0D1B63),
+                      ),
+                      child: _icons[index],
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
